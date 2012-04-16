38ff68670aed484a87af3a23a3be60da46940a29
James Halliday <mail@substack.net>
2012-04-15 23:51:52 -0700

    ported nest next test to tap

--- test/chainsaw.js
+++ test/chainsaw.js
@@ -1,62 +1,6 @@
 var test = require('tap').test;
 var chainsaw = require('../');
 
-test('nestNext', function (t) {
-    var ch = (function () {
-        var vars = {};
-        return chainsaw(function (saw) {
-            this.do = function (cb) {
-                saw.nest(false, function () {
-                    var args = [].slice.call(arguments);
-                    args.push(saw.next);
-                    cb.apply(this, args);
-                }, vars);
-            };
-        });
-    })();
-    
-    var order = [];
-    var to = setTimeout(function () {
-        t.fail("Didn't get to the end");
-    }, 500);
-    
-    var times = [];
-    
-    ch
-        .do(function (vars, next_) {
-            vars.x = 'y';
-            order.push(1);
-            
-            this
-                .do(function (vs, next) {
-                    order.push(2);
-                    vs.x = 'x';
-                    setTimeout(next, 30);
-                })
-                .do(function (vs, next) {
-                    order.push(3);
-                    vs.z = 'z';
-                    setTimeout(next, 10);
-                })
-                .do(function () {
-                    setTimeout(next_, 20);
-                })
-            ;
-        })
-        .do(function (vars, next) {
-            vars.y = 'y';
-            order.push(4);
-            setTimeout(next, 5);
-        })
-        .do(function (vars) {
-            t.same(order, [1,2,3,4]);
-            t.same(vars, { x : 'x', y : 'y', z : 'z' });
-            
-            clearTimeout(to);
-        })
-    ;
-});
-
 test('builder', function (t) {
     var cx = chainsaw(function (saw) {
         this.x = function () {};
